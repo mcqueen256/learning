0c22cf677ebd957d38ecbf91c3f6e0487aec6156
Nicholas Buckeridge <bucknich@gmail.com>
2017-12-27 21:56:31 +1000

    added 1.2.3 but empty

--- rust/rbe/rbe_1_2_2_1/src/main.rs
+++ rust/rbe/rbe_1_2_2_1/src/main.rs
@@ -6,7 +6,7 @@ impl fmt::Display for List {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let vec = &self.0;
 
-        write!(f, "[]")?;
+        write!(f, "[")?;
 
         for (count, v) in vec.iter().enumerate() {
             if count != 0 { write!(f, ", ")?; }
@@ -16,8 +16,25 @@ impl fmt::Display for List {
     }
 }
 
+struct ListX(Vec<i32>);
+
+impl fmt::Display for ListX {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[");
+        for (i, elm) in self.0.iter().enumerate() {
+            if i != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}: {}", i, elm)?;
+        }
+        write!(f, "]")
+    }
+}
+
 
 fn main() {
     let v = List(vec![1, 2, 3]);
-    println!("{}", v);
+    println!("v: {}", v);
+    let x = ListX(vec![1,0,2,9,3,8,4,7,5,6]);
+    println!("x: {}", x);
 }
