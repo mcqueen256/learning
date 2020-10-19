cf7884d477fba29b5e991e6004212e4fc547004d
Nicholas Buckeridge <bucknich@gmail.com>
2020-10-20 08:16:32 +1000

    finished rust ch14

---
+++ rust/The_Rust_Programming_Language/ch14/cargo_docs/src/lib.rs
@@ -0,0 +1,17 @@
+//! # This is general documentation
+//! test one two three.
+
+
+
+/// Adds one to the given number
+/// 
+/// # Example
+/// ```
+/// let arg = 5;
+/// let answer = ch14::add_one(arg);
+/// 
+/// assert_eq!(answer, 6);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
